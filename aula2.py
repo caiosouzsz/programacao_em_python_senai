20494af9e4f86c33d3df4560ae3354a5781a6cda
caiosouzsz <caio.souza.168ds@gmail.com>
2025-10-04 17:55:23 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,12 @@
+num1 = int(input("digite um número"))
+num2 = int(input('digie outro'))
+
+print('soma =1')
+print('subtração = 2')
+print('multiplição = 3')
+print('divisão = 4')
+operacao = int(input('escolha a operacao'))
+
+if(operacao == 1):soma = num1 + num2
+print(soma)
+
