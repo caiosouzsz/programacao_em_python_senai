a174aa41c8906bd3fcb5a841532b816339c782f1
caiosouzsz <caio.souza.168ds@gmail.com>
2025-10-05 00:40:39 -0300

    Add files via upload
    Calculadora simples com python

---
+++ AULA_1_ALGORITMOS/aula2.py
@@ -0,0 +1,23 @@
+#variaveis
+num1 = float(input("digite um número: "))
+num2 = float(input('digie outro: '))
+soma = float
+sub = float
+multi = float
+divi = float
+#operações
+print('soma = 1')
+print('subtração = 2')
+print('multiplição = 3')
+print('divisão = 4')
+operacao = int(input('escolha a operacao: '))
+#escolhas 
+if(operacao == 1):soma = num1 + num2; print('o seu resultado é: ',(soma))
+#fim do bloco if
+if(operacao == 2):sub = num1 - num2; print('o seu resultado é: ',(sub))
+#fim do bloco if
+if(operacao == 3):multi = num1 * num2; print('o seu resultado é: ',(multi))
+#fim do bloco if
+if(operacao == 4):divi = num1 / num2; print('o seu resultado é: ',(divi))
+
+
