96d51eb80df035d0c24edb82354cdb436efa375e
caiosouzsz <caio.souza.168ds@gmail.com>
2025-10-11 12:08:59 -0300

    Create AULA_2.py

---
+++ AULA_2.py
@@ -0,0 +1,17 @@
+num1 = int(input('escreva um número: '))
+print('o seu resultado é: ',num1 ** 2)
+
+prim_nome = input('digite seu primeiro nome: ')
+segun_nome = input('digite seu segundo nome: ')
+print('seu nome é: ', prim_nome, segun_nome)
+
+n1 = int(input('digite um número inteiro: '))
+n2 = int(input('digite outro: '))
+print('a concatenação desses números é: ', str(n1) + str(n2))
+
+py = int(input('digite um número para ser exibido ao lado a palavra python: '))
+print('python' + str(py))
+
+frase = 'olá mundo'
+palavra = input('digite uma palavra para ser exibida ao lado da frase: ')
+print(str(frase) + str(palavra))
